c2bdc2878bffe3c030bf2f168efe519c92012a54
sbkim28 <sbkim28@naver.com>
2018-11-02 23:55:48 +0900

    add javadoc

--- src/com/ignited/skld/KoreanWord.java
+++ src/com/ignited/skld/KoreanWord.java
@@ -5,6 +5,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 
+/**
+ * Korean Word
+ *
+ * @author Ignited
+ */
 public class KoreanWord implements IWord{
 
     private final int homonym;
@@ -19,18 +24,36 @@ public class KoreanWord implements IWord{
         this.properties = Collections.unmodifiableList(builder.properties);
     }
 
+    /**
+     * Gets index of homonym.
+     *
+     * @return the index of homonym
+     */
     public int getHomonym() {
         return homonym;
     }
+
 
     public String getWord() {
         return word;
     }
 
-    public boolean isContainsOldHangeul() {
+    /**
+     * If the word contains old hangeul, it returns true. Else, it returns false.
+     * That is, it returns whether or not the word is composed of ㄱ~ㅎ, ㅏ~ㅣ or 가~힣.
+     *
+     * @return whether the word contains old hangeul or not
+     */
+    public boolean containsOldHangeul() {
         return containsOldHangeul;
     }
 
+    /**
+     * Gets properties of the word.
+     *
+     * @return the properties
+     * @see com.ignited.skld.Property
+     */
     public List<Property> getProperties() {
         return properties;
     }
@@ -45,36 +68,67 @@ public class KoreanWord implements IWord{
                 '}';
     }
 
+    /**
+     * The Korean World Builder
+     * Instantiates a new KoreanWord
+     */
     public static final class Builder {
         private int homonym;
         private String word;
         private boolean containsOldHangeul;
         private final List<Property> properties;
 
+        /**
+         * Instantiates a new Builder.
+         */
         public Builder() {
             properties = new ArrayList<>();
         }
 
+        /**
+         * Set the index of homonym
+         *
+         * @param homonym the homonym
+         * @return this
+         */
         public Builder setHomonym(int homonym){
             this.homonym = homonym;
             return this;
         }
 
+        /**
+         * Set the Korean Word.
+         * Also checks if the word contains old Hangeul.
+         *
+         * @param word the word
+         * @return this
+         */
         public Builder setWord(String word){
             this.word = word;
-            containsOldHangeul = isContainsOldHangeul(word);
+            containsOldHangeul = containsOldHangeul(word);
             return this;
         }
 
-        private boolean isContainsOldHangeul(String word){
-            return !Pattern.matches("^[ㄱ-ㅎ가-힣]*$", word);
+        private boolean containsOldHangeul(String word){
+            return !Pattern.matches("^[ㄱ-ㅎㅏ-ㅣ가-힣]*$", word);
         }
 
+        /**
+         * Add the property of the word
+         *
+         * @param property the property
+         * @return this
+         */
         public Builder addProperty(Property property){
             properties.add(property);
             return this;
         }
 
+        /**
+         * Build korean word.
+         *
+         * @return the korean word
+         */
         public KoreanWord build(){
             return new KoreanWord(this);
         }
--- src/com/ignited/skld/Property.java
+++ src/com/ignited/skld/Property.java
@@ -5,10 +5,16 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+
+/**
+ * The Word Property
+ *
+ * @author Ignited
+ */
 public class Property{
     private final String wordClass;
     private final List<String> attributes;
-    private final List<List<String>> meanings;
+    private final List<String> meanings;
 
     private Property(Builder builder) {
         this.wordClass = builder.wordClass;
@@ -16,10 +22,20 @@ public class Property{
         this.meanings = Collections.unmodifiableList(builder.meanings);
     }
 
+    /**
+     * Gets word class.
+     *
+     * @return the word class
+     */
     public String getWordClass() {
         return wordClass;
     }
 
+    /**
+     * Gets the list of attributes.
+     *
+     * @return the unmodifiable list of attributes
+     */
     public List<String> getAttributes() {
         return attributes;
     }
@@ -33,44 +49,78 @@ public class Property{
                 '}';
     }
 
+    /**
+     * The Property Builder.
+     * Instantiates a new Property
+     */
     public static class Builder{
         private String wordClass;
         private final List<String> attributes;
-        private final List<List<String>> meanings;
+        private final List<String> meanings;
 
         private final List<String> m;
 
+        /**
+         * Instantiates a new Builder.
+         */
         public Builder() {
             attributes = new ArrayList<>();
             meanings = new ArrayList<>();
             m = new ArrayList<>();
         }
 
+        /**
+         * Set word class
+         *
+         * @param wordClass the word class
+         * @return this
+         */
         public Builder setWordClass(String wordClass){
             this.wordClass = wordClass;
             return this;
         }
 
+        /**
+         * Add an attribute of the word
+         *
+         * @param attribute an attribute of the word
+         * @return this
+         */
         public Builder addAttribute(String attribute){
             this.attributes.add(attribute);
             return this;
         }
 
+        /**
+         * Add a collection of attributes of the word
+         *
+         * @param attributes the attributes
+         * @return this
+         */
         public Builder addAllAtrribute(Collection<String> attributes){
             this.attributes.addAll(attributes);
             return this;
         }
 
-        public Builder next(){
-            meanings.add(Collections.unmodifiableList(new ArrayList<>(m)));
-            m.clear();
-            return this;
-        }
+        @Deprecated
+        public Builder next(){ return this;}
+
+        /**
+         * Add a meaning of the word.
+         *
+         * @param meaning a meaning of the word
+         * @return this
+         */
         public Builder addMeaning(String meaning){
             m.add(meaning);
             return this;
         }
 
+        /**
+         * Build the property of the word.
+         *
+         * @return the property of the word
+         */
         public Property build(){
             return new Property(this);
         }
